2897da9168e9dbfdc0831d619402b6c7e0fa0671
Elif Sanem Ceyhan <270886456+sanerifu@users.noreply.github.com>
2026-04-10 17:33:25 +0300

    Add simple arena

---
+++ arena.c
@@ -0,0 +1,55 @@
+#ifndef __ARENA_C__
+#define __ARENA_C__
+
+#include "result.c"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Arena* Arena;
+struct Arena {
+    Arena prev;
+    size_t size;
+    size_t capacity;
+    char data[];
+};
+
+static Result arenaAllocate(void* o_ret, Arena* io_self, size_t size) {
+    Arena self = *io_self;
+    if(self == NULL) {
+        self = malloc(sizeof(struct Arena) + size);
+        if(self == NULL) {
+            return OUT_OF_MEMORY;
+        }
+        self->capacity = size;
+        self->size = 0;
+        self->prev = NULL;
+    } else if(self->capacity - self->size < size) {
+        size_t new_size = size > self->capacity * 2 ? size : self->capacity * 2;
+        Arena new_node = malloc(sizeof(struct Arena) + new_size);
+        if(new_node == NULL) {
+            return OUT_OF_MEMORY;
+        }
+        new_node->capacity = new_size;
+        new_node->size = 0;
+        new_node->prev = self;
+        self = new_node;
+    }
+
+    *(void**)o_ret = self->data + self->size;
+    self->size += size;
+    *io_self = self;
+    return SUCCESS;
+}
+
+static void arenaDestroy(Arena* io_self) {
+    Arena self = *io_self;
+    while(self != NULL) {
+        Arena prev = self->prev;
+        free(self);
+        self = prev;
+    }
+    *io_self = NULL;
+}
+
+#endif
