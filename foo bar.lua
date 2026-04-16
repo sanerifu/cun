d32a67d6384bd646f58ce4482d3a4d80e35407a0
Elif Sanem Ceyhan <270886456+sanerifu@users.noreply.github.com>
2026-04-16 15:57:51 +0300

    HTML for test?

--- foo bar.lua
+++ foo bar.lua
@@ -4,6 +4,17 @@ print(inspect.inspect(request))
 
 return {
     status = 200,
-    content_type = "text/plain",
-    body = ("Hello, %s!\n"):format(request.queries.name or "stranger")
+    content_type = "text/html",
+    body = ([[
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8" />
+            <title>Foobar</title>
+        </head>
+        <body>
+            <h1>Hello, %s!</h1>
+        </body>
+        </html>
+    ]]):format(request.queries.name or "stranger")
 }
